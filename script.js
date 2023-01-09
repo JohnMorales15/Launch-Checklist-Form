eb0514abea2734359b6a55c9fe6a41f0dd3f9f97
John Morales <john@Johns-MacBook-Pro.local>
2023-01-09 12:57:36 -0700

    isNaN for Fuel level and Cargo mass

--- script.js
+++ script.js
@@ -18,12 +18,15 @@ window.addEventListener("load", function() {
    let form = this.document.getElementById("launchForm");
    form.addEventListener("submit", function(event){
       let pilotName = document.querySelector("input[name=pilotName]");
-      let copilot = document.querySelector("input[name=copilot]");
+      let copilot = document.querySelector("input[name=copilotName]");
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
       let cargoMass = document.querySelector("input[name=cargoMass]");
       if (pilotName.value === "" || copilot.value === "" || fuelLevel.value === "" || cargoMass.value === ""){
          alert("All fields are required!")
          event.preventDefault();
-      } 
+      } else if (isNaN(fuelLevel.value) === true || isNaN(cargoMass.value) === true){
+         alert("Please input numerical value for Fuel level and Cargo Mass");
+         event.preventDefault();
+      }
    })
 })
